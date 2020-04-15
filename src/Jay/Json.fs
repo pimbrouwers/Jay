8ddd00237764921de290ecb649076f92e0389d63
Pim Brouwers <pbrouwers@nhlpa.com>
2020-04-15 17:26:50 -0400

    hello world

---
+++ src/Jay/Json.fs
@@ -0,0 +1,189 @@
+﻿module Jay
+
+open System
+open System.Globalization
+open System.IO
+open System.Text
+
+type Json =
+  | String  of string
+  | Number  of decimal
+  | Float   of float
+  | Record  of properties:(string * Json)[]
+  | Array   of elements:Json[]
+  | Boolean of bool
+  | Null  
+
+type private JsonParser(jsonText:string) =
+
+    let mutable i = 0
+    let s = jsonText
+    
+    let buf = StringBuilder() // pre-allocate buffers for strings
+
+    // Helper functions
+    let skipWhitespace() =
+      while i < s.Length && Char.IsWhiteSpace s.[i] do
+        i <- i + 1
+    let isNumChar c =
+      Char.IsDigit c || c = '.' || c='e' || c='E' || c='+' || c='-'
+    let throw() =
+      let msg =
+        sprintf
+          "Invalid JSON starting at character %d, snippet = \n----\n%s\n-----\njson = \n------\n%s\n-------" 
+          i (jsonText.[(max 0 (i-10))..(min (jsonText.Length-1) (i+10))]) (if jsonText.Length > 1000 then jsonText.Substring(0, 1000) else jsonText)
+      failwith msg
+    let ensure cond =
+      if not cond then throw()
+
+    // Recursive descent parser for JSON that uses global mutable index
+    let rec parseValue() =
+        skipWhitespace()
+        ensure(i < s.Length)
+        match s.[i] with
+        | '"' -> Json.String(parseString())
+        | '-' -> parseNum()
+        | c when Char.IsDigit(c) -> parseNum()
+        | '{' -> parseObject()
+        | '[' -> parseArray()
+        | 't' -> parseLiteral("true", Json.Boolean true)
+        | 'f' -> parseLiteral("false", Json.Boolean false)
+        | 'n' -> parseLiteral("null", Json.Null)
+        | _ -> throw()
+
+    and parseString() =
+        ensure(i < s.Length && s.[i] = '"')
+        i <- i + 1
+        while i < s.Length && s.[i] <> '"' do
+            if s.[i] = '\\' then
+                ensure(i+1 < s.Length)
+                match s.[i+1] with
+                | 'b' -> buf.Append('\b') |> ignore
+                | 'f' -> buf.Append('\f') |> ignore
+                | 'n' -> buf.Append('\n') |> ignore
+                | 't' -> buf.Append('\t') |> ignore
+                | 'r' -> buf.Append('\r') |> ignore
+                | '\\' -> buf.Append('\\') |> ignore
+                | '/' -> buf.Append('/') |> ignore
+                | '"' -> buf.Append('"') |> ignore
+                | 'u' ->
+                    ensure(i+5 < s.Length)
+                    let hexdigit d =
+                        if d >= '0' && d <= '9' then int32 d - int32 '0'
+                        elif d >= 'a' && d <= 'f' then int32 d - int32 'a' + 10
+                        elif d >= 'A' && d <= 'F' then int32 d - int32 'A' + 10
+                        else failwith "hexdigit"
+                    let unicodeChar (s:string) =
+                        if s.Length <> 4 then failwith "unicodeChar";
+                        char (hexdigit s.[0] * 4096 + hexdigit s.[1] * 256 + hexdigit s.[2] * 16 + hexdigit s.[3])
+                    let ch = unicodeChar (s.Substring(i+2, 4))
+                    buf.Append(ch) |> ignore
+                    i <- i + 4  // the \ and u will also be skipped past further below
+                | 'U' ->
+                    ensure(i+9 < s.Length)
+                    let unicodeChar (s:string) =
+                        if s.Length <> 8 then failwith "unicodeChar";
+                        if s.[0..1] <> "00" then failwith "unicodeChar";
+                        UnicodeHelper.getUnicodeSurrogatePair <| UInt32.Parse(s, NumberStyles.HexNumber) 
+                    let lead, trail = unicodeChar (s.Substring(i+2, 8))
+                    buf.Append(lead) |> ignore
+                    buf.Append(trail) |> ignore
+                    i <- i + 8  // the \ and u will also be skipped past further below
+                | _ -> throw()
+                i <- i + 2  // skip past \ and next char
+            else
+                buf.Append(s.[i]) |> ignore
+                i <- i + 1
+        ensure(i < s.Length && s.[i] = '"')
+        i <- i + 1
+        let str = buf.ToString()
+        buf.Clear() |> ignore
+        str
+
+    and parseNum() =
+        let start = i
+        while i < s.Length && (isNumChar s.[i]) do
+            i <- i + 1
+        let len = i - start
+        let sub = s.Substring(start,len)
+        match TextConversions.parseDecimal CultureInfo.InvariantCulture sub with
+        | Some x -> Json.Number x
+        | _ ->
+            match TextConversions.parseFloat CultureInfo.InvariantCulture sub with
+            | Some x -> Json.Float x
+            | _ -> throw()
+
+    and parsePair() =
+        let key = parseString()
+        skipWhitespace()
+        ensure(i < s.Length && s.[i] = ':')
+        i <- i + 1
+        skipWhitespace()
+        key, parseValue()
+
+    and parseObject() =
+        ensure(i < s.Length && s.[i] = '{')
+        i <- i + 1
+        skipWhitespace()
+        let pairs = ResizeArray<_>()
+        if i < s.Length && s.[i] = '"' then
+            pairs.Add(parsePair())
+            skipWhitespace()
+            while i < s.Length && s.[i] = ',' do
+                i <- i + 1
+                skipWhitespace()
+                pairs.Add(parsePair())
+                skipWhitespace()
+        ensure(i < s.Length && s.[i] = '}')
+        i <- i + 1
+        Json.Record(pairs.ToArray())
+
+    and parseArray() =
+        ensure(i < s.Length && s.[i] = '[')
+        i <- i + 1
+        skipWhitespace()
+        let vals = ResizeArray<_>()
+        if i < s.Length && s.[i] <> ']' then
+            vals.Add(parseValue())
+            skipWhitespace()
+            while i < s.Length && s.[i] = ',' do
+                i <- i + 1
+                skipWhitespace()
+                vals.Add(parseValue())
+                skipWhitespace()
+        ensure(i < s.Length && s.[i] = ']')
+        i <- i + 1
+        Json.Array(vals.ToArray())
+
+    and parseLiteral(expected, r) =
+        ensure(i+expected.Length <= s.Length)
+        for j in 0 .. expected.Length - 1 do
+            ensure(s.[i+j] = expected.[j])
+        i <- i + expected.Length
+        r
+
+    // Start by parsing the top-level value
+    member _.Parse() =
+        let value = parseValue()
+        skipWhitespace()
+        if i <> s.Length then
+            throw()
+        value
+
+module Json =
+    let parse (str : string) = 
+        JsonParser(str).Parse()
+    
+    let tryParse (str : string) = 
+        trySome (JsonParser(str).Parse())
+
+    let parseStream (str : Stream) =
+        use reader = new StreamReader(str)
+        let text = reader.ReadToEnd()
+        JsonParser(text).Parse()
+
+    let tryParseStream (str : Stream) =
+        trySome (fun _ -> 
+            use reader = new StreamReader(str)
+            let text = reader.ReadToEnd()
+            JsonParser(text).Parse())
